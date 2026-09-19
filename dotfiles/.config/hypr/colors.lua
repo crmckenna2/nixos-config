42470349e04196f4dec3570c1418a55c1686936a
Cody McKenna <crmckenna2@wisc.edu>
2026-09-18 21:56:51 -0500

    Got a good baseline hyprland config running.

--- dotfiles/.config/hypr/colors.lua
+++ dotfiles/.config/hypr/colors.lua
@@ -1,3 +1,23 @@
 -------------------------
 ---- COLORS & THEMES ----
 -------------------------
+
+hl.config({
+  general = {
+    
+    col = {
+       active_border   = { colors = {"rgba(33ccffee)", "rgba(00ff99ee)"}, angle = 45 },
+       inactive_border = "rgba(595959aa)",
+    },
+
+  },
+
+  decoration = {
+
+    shadow = {
+      color = 0xee1a1a1a,
+    },
+
+  },
+
+})
