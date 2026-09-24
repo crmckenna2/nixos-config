d2ffed6b7ac8e413761997628b046a9db7743205
Cody McKenna <crmckenna2@wisc.edu>
2026-09-24 16:09:16 -0500

    Added laptop monitor to hyprland config.

--- dotfiles/.config/hypr/input-output.lua
+++ dotfiles/.config/hypr/input-output.lua
@@ -10,6 +10,13 @@ hl.monitor({
 })
 
 hl.monitor({
+  output = "eDP-1",
+  mode = "1920x1200@59.95000",
+  position = "0x0",
+  scale = 1,
+})
+
+hl.monitor({
   output = "",
   mode = "preferred",
   position = "auto",
